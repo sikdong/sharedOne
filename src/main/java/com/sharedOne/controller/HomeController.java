d59f264c9e4bd055ba85197a134df0d5e8124cbb
yoob005 <yoob005@gmail.com>
2023-01-11 11:28:45 +0900

    home add

---
+++ src/main/java/com/sharedOne/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.sharedOne.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("")		
+	public String home() {
+		
+		return "redirect:/get";
+	}
+	
+	@RequestMapping("get")
+	public void get() {
+		
+	}
+}
